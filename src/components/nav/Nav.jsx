93730aa3614d89fd8d544794d6fe67c9890db103
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-28 15:41:38 +0530

    style : navigation bar

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,25 @@
 import React from "react";
-
+import "./Nav.css";
 const Nav = () => {
-  return <div>Nav</div>;
+  return (
+    <nav>
+      <div className="logo">
+        SPY
+        <i class="fa-brands fa-reddit"></i>SPOT
+      </div>
+      <div className="search-div">
+        <input className="search" placeholder="search" />
+        <div>
+          <i className="fa-solid fa-magnifying-glass"></i>
+          {/* <i class="fa-solid fa-microphone-lines"></i> */}
+        </div>
+      </div>
+      <button className="btn">
+        LOGIN
+        <i class="fa-solid fa-circle-user"></i>
+      </button>
+    </nav>
+  );
 };
 
 export { Nav };
--- src/pages/videos-listing/VideoListing.jsx
+++ src/pages/videos-listing/VideoListing.jsx
@@ -5,7 +5,6 @@ const VideoListing = () => {
   return (
     <div>
       <Nav />
-      <h1>Video listing</h1>
     </div>
   );
 };
