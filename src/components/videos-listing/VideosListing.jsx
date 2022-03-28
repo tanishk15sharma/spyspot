8d27f319f2576e1058375f1030540e019464d88e
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-29 00:00:11 +0530

    style : chips

--- src/components/videos-listing/VideosListing.jsx
+++ src/components/videos-listing/VideosListing.jsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { SuggestionChips } from "./SuggestionChips";
 import "./VideosListing.css";
 const VideosListing = () => {
-  return <div className="hero-div">VideosListing</div>;
+  return (
+    <div className="hero-div">
+      <SuggestionChips />
+    </div>
+  );
 };
 
 export { VideosListing };
