fed249958af747b3fd6181735e373da6470ff18b
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-29 02:53:27 +0530

    style : video-options

--- src/components/videos-listing/VideoCard.jsx
+++ src/components/videos-listing/VideoCard.jsx
@@ -1,15 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import img from "../../../src/components/hq720.webp";
 import "./VideoCard.css";
 const VideoCard = () => {
+  const [toggleOptions, setToggleOptions] = useState(false);
   return (
-    <div className="video-card">
+    <div className="video-card relative">
       <div className="video-img-div">
         <img src={img} className="video-thumbnail" />
       </div>
       <div className="pd-sm flex-sb">
         <div className="video-title">TANISHK is looking winnerr in</div>
-        <i class="fa-solid fa-ellipsis-vertical"></i>
+        <i
+          class="fa-solid fa-ellipsis-vertical"
+          onClick={() => setToggleOptions((val) => !val)}
+        ></i>
+      </div>
+      <div
+        className="video-options"
+        style={{ display: !toggleOptions && "none" }}
+      >
+        <div className="option">
+          <i class="fa-solid fa-clock-rotate-left margin-r-sm"></i>
+          Save to Watch Later
+        </div>
+        <div className="option">
+          <i class="fa-solid fa-thumbs-up margin-r-sm"></i>
+          Like Video
+        </div>
       </div>
       <div className="pd-sm video-type">Dunk</div>
     </div>
