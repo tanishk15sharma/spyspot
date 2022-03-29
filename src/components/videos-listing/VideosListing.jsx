70b84fd1e36cce2c5337f895847685832cf721cd
tanishk15sharma <tanishk1582002@gmail.com>
2022-03-29 12:52:15 +0530

    feat : Getting videos data from API

--- src/components/videos-listing/VideosListing.jsx
+++ src/components/videos-listing/VideosListing.jsx
@@ -1,19 +1,29 @@
-import React from "react";
+import axios from "axios";
+import React, { useState, useEffect } from "react";
 import { SuggestionChips } from "./SuggestionChips";
 import { VideoCard } from "./VideoCard";
 import "./VideosListing.css";
 const VideosListing = () => {
+  const [videosData, setVideosData] = useState([]);
+
+  useEffect(() => {
+    (async () => {
+      try {
+        const { data } = await axios.get("/api/videos");
+        setVideosData(data.videos);
+      } catch (err) {
+        console.log(err);
+      }
+    })();
+  }, []);
+  console.log(videosData);
   return (
     <div className="hero-div">
       <SuggestionChips />
       <div className="videos-wrapper">
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
-        <VideoCard />
+        {videosData.map((video) => (
+          <VideoCard video={video} key={video._id} />
+        ))}
       </div>
     </div>
   );
