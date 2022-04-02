1af37342d6f685200c6f2899e12c0c3da81770d8
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-02 12:07:42 +0530

    style : history page

--- src/components/videos-history/HistoryVideos.jsx
+++ src/components/videos-history/HistoryVideos.jsx
@@ -1,6 +1,8 @@
 import React from "react";
+import { useUserVideos } from "../../contexts/user-videos";
 import "./HistoryVideos.css";
 const HistoryVideos = () => {
+  const { state } = useUserVideos();
   return (
     <div className="history-container">
       <div className="history-info">
@@ -8,22 +10,27 @@ const HistoryVideos = () => {
         <div>NO. </div>
         <button className="btn">CLEAR ALL </button>
       </div>
-      <div className="history-lists">
-        <div className="flex-space-btw">
-          <div>
-            <div className="flex-space-btw">
-              <img
-                src={`https://i.ytimg.com/vi/aaaa/maxresdefault.jpg`}
-                className="full-img"
-              />
-              <div>
-                <h2>title</h2>
-                <h2>category</h2>
+      <div className="wrap">
+        {state.history.map((video) => (
+          <div className="history-list">
+            <div className="history-video">
+              <div className="flex-history">
+                <div className="xl-img">
+                  <img
+                    src={`https://i.ytimg.com/vi/${video._id}/maxresdefault.jpg`}
+                    className="full-img"
+                  />
+                </div>
+                <div>
+                  <h2>{video.title}</h2>
+                  <h2 className="light-name">{video.category}</h2>
+                </div>
               </div>
+
+              <i className="fa-solid fa-trash"></i>
             </div>
           </div>
-          <div>icon</div>
-        </div>
+        ))}
       </div>
     </div>
   );
