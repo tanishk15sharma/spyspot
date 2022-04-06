a44c1a9c42aa65bcd0dca715bac12452db30a1a7
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-07 02:28:27 +0530

    feat : valid signup

--- src/utilities/auth-utils.jsx
+++ src/utilities/auth-utils.jsx
@@ -1,3 +1,10 @@
+import {
+  EMAIL_REGEX,
+  PASSWORD_LOWERCASE_REGEX,
+  PASSWORD_UPPERCASE_REGEX,
+  PASSWORD_NUMBER_REGEX,
+} from "./helper-utils";
+
 const validLogin = ({ email, password }, initialErrors) => {
   if (!email)
     return {
@@ -19,4 +26,84 @@ const validLogin = ({ email, password }, initialErrors) => {
   };
 };
 
-export { validLogin };
+const validSignUp = (
+  { firstName, lastName, email, password, confirmPassword, terms },
+  initialErrors
+) => {
+  if (!firstName)
+    return {
+      isValid: false,
+      errors: { ...initialErrors, firstName: "First Name is Required" },
+    };
+  if (!lastName)
+    return {
+      isValid: false,
+      errors: { ...initialErrors, lastName: "Last Name is Required" },
+    };
+  if (!email)
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        email: "Email is Required",
+      },
+    };
+  if (!EMAIL_REGEX.test(email))
+    return {
+      isValid: false,
+      errors: { ...initialErrors, email: "Enter a valid Email Address" },
+    };
+  if (!PASSWORD_UPPERCASE_REGEX.test(password))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        password: "Password must contain atleast 1 Uppercase Letter",
+      },
+    };
+  if (!PASSWORD_LOWERCASE_REGEX.test(password))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        password: "Password must contain atleast 1 Lowercase Letter",
+      },
+    };
+  if (!PASSWORD_NUMBER_REGEX.test(password))
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        password: "Password must contain atleast 1 number ",
+      },
+    };
+  if (password !== confirmPassword)
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        confirmPassword: "Password does not match",
+      },
+    };
+  if (!terms)
+    return {
+      isValid: false,
+      errors: {
+        ...initialErrors,
+        terms: "Accept Terms & Conditions",
+      },
+    };
+  return {
+    isValid: true,
+    errors: {
+      firstName: "",
+      lastName: "",
+      mobile: "",
+      password: "",
+      confirmPassword: "",
+      email: "",
+    },
+  };
+};
+
+export { validLogin, validSignUp };
