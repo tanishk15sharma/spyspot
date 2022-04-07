7aa226dbd22bd50ff0c4e2b69cfd9a73d395d551
tanishk15sharma <tanishk1582002@gmail.com>
2022-04-07 12:22:33 +0530

    feat : avtice nav

--- src/components/sidebar/Sidebar.jsx
+++ src/components/sidebar/Sidebar.jsx
@@ -1,53 +1,73 @@
 import React, { useState } from "react";
-import { Link } from "react-router-dom";
-import { Menu } from "./Menu";
+
+import { NavLink } from "react-router-dom";
 import "./Sidebar.css";
 const Sidebar = () => {
-  const [toggleMenu, setToggleMenu] = useState(false);
+  const getActiveStyleBar = ({ isActive }) => ({
+    backgroundColor: isActive ? "#353535" : "",
+  });
+
   return (
     <div className="sidebar">
-      {/* <div className="hamburger" onClick={() => setToggleMenu(true)}>
-        <span class="material-icons side-icon xxl">menu</span>
-        <Menu menuVal={toggleMenu} setMenu={() => setToggleMenu(false)} />
-      </div> */}
       <div className="side-nav">
-        <Link to="/">
-          <div className="side-icon-div">
-            <span className="material-icons side-icon">home</span>
-            <p>Home</p>
-          </div>
-        </Link>
-        <Link to="/videos">
-          <div className="side-icon-div">
-            <span className="material-icons side-icon">explore</span>
-            <p>Explore</p>
-          </div>
-        </Link>
-        <Link to="/playlists">
-          <div className="side-icon-div">
-            <span className="material-icons side-icon">create_new_folder</span>
-            <p>Playlist</p>
-          </div>
-        </Link>
-        <Link to="/likes">
-          <div className="side-icon-div">
-            <span className="material-icons side-icon">thumb_up</span>
-            <p>Likes</p>
-          </div>
-        </Link>
-        <Link to="/history">
-          <div className="side-icon-div">
-            <span className="material-icons side-icon">history</span>
+        <NavLink to="/" style={getActiveStyleBar} className="side-icon-div">
+          {/* <div className="side-icon-div"> */}
+          <span className="material-icons side-icon">home</span>
+          <p>Home</p>
+          {/* </div> */}
+        </NavLink>
 
-            <p>History</p>
-          </div>
-        </Link>
-        <Link to="/watchlater">
-          <div className="side-icon-div">
-            <span className="material-icons side-icon">play_lesson</span>
-            <p>Watch Later</p>
-          </div>
-        </Link>
+        <NavLink
+          to="/videos"
+          style={getActiveStyleBar}
+          className="side-icon-div"
+        >
+          {/* <div className="side-icon-div"> */}
+          <span className="material-icons side-icon">explore</span>
+          <p>Explore</p>
+          {/* </div> */}
+        </NavLink>
+        <NavLink
+          to="/playlists"
+          style={getActiveStyleBar}
+          className="side-icon-div"
+        >
+          {/* <div className="side-icon-div"> */}
+          <span className="material-icons side-icon">create_new_folder</span>
+          <p>Playlist</p>
+          {/* </div> */}
+        </NavLink>
+        <NavLink
+          to="/likes"
+          style={getActiveStyleBar}
+          className="side-icon-div"
+        >
+          {/* <div className="side-icon-div"> */}
+          <span className="material-icons side-icon">thumb_up</span>
+          <p>Likes</p>
+          {/* </div> */}
+        </NavLink>
+        <NavLink
+          to="/history"
+          style={getActiveStyleBar}
+          className="side-icon-div"
+        >
+          {/* <div className="side-icon-div"> */}
+          <span className="material-icons side-icon">history</span>
+
+          <p>History</p>
+          {/* </div> */}
+        </NavLink>
+        <NavLink
+          to="/watchlater"
+          style={getActiveStyleBar}
+          className="side-icon-div"
+        >
+          {/* <div className="side-icon-div"> */}
+          <span className="material-icons side-icon">play_lesson</span>
+          <p>Watch Later</p>
+          {/* </div> */}
+        </NavLink>
       </div>
     </div>
   );
