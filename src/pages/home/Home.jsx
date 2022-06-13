d686bdc64f4520e128561e919567818c069cd2ec
tanishk15sharma <tanishk1582002@gmail.com>
2022-06-14 02:37:52 +0530

    style:landing page

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,6 +1,7 @@
 import React, { useEffect } from "react";
 import { useFilters } from "../../contexts/filters";
 import { Link, useNavigate } from "react-router-dom";
+import homeVideo from "../../assets/videoplayback.mp4";
 import axios from "axios";
 import "./Home.css";
 const Home = () => {
@@ -19,13 +20,16 @@ const Home = () => {
   }, []);
 
   return (
-    <div className="home-top">
-      <div className="home-logo">
-        SPY
-        <i className="fa-brands fa-reddit"></i>SPOT
-      </div>
-      <div className="home">
-        <aside className="home-aside">
+    <>
+      <video autoPlay muted loop className="landing-bg-video">
+        <source src={homeVideo} type="video/mp4" />
+      </video>
+      <main className="banner">
+        <div className="landing-info">
+          <div className="home-logo">
+            SPY
+            <i className="fa-brands fa-reddit"></i>SPOT
+          </div>
           <p className="home-txt">
             Life In The Fast Lane, Let the{" "}
             <span className="highlight-txt">MADNESS</span> begin
@@ -33,55 +37,45 @@ const Home = () => {
           </p>
           <Link to="/videos">
             <button
-              className="btn btn-round"
+              className="landing-btn"
               onClick={() =>
-                filterDispatch({ type: "SET_ACTIVE_BUTTON", payload: "All" })
+                filterDispatch({
+                  type: "SET_ACTIVE_BUTTON",
+                  payload: "All",
+                })
               }
             >
               EXPLORE
             </button>
           </Link>
-        </aside>
-        <div className="home-video">
-          <div className="video">
-            <iframe
-              className="video-iframe"
-              src="https://www.youtube.com/embed/a8Ng2btsXeI?autoplay=1&amp;loop=1&amp;mute=1&amp;enablejsapi=1&amp;controls=0&amp;start=193"
-              title="YouTube video player"
-              frameBorder="0"
-              allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
-              allowFullScreen
-            ></iframe>
-          </div>
-          <div>
-            <div className="category-box">
-              {filterState.categories.map(
-                ({ categoryName, _id }, index) =>
-                  index < 4 && (
-                    <div
-                      key={_id}
-                      className="home-category"
-                      onClick={() => {
-                        filterDispatch({
-                          type: "CATEGORY",
-                          payload: categoryName,
-                        });
-                        filterDispatch({
-                          type: "SET_ACTIVE_BUTTON",
-                          payload: _id,
-                        });
-                        navigate("/videos");
-                      }}
-                    >
-                      {categoryName}
-                    </div>
-                  )
-              )}
-            </div>
+
+          <div className="category-box">
+            {filterState.categories.map(
+              ({ categoryName, _id }, index) =>
+                index < 4 && (
+                  <div
+                    key={_id}
+                    className="home-category"
+                    onClick={() => {
+                      filterDispatch({
+                        type: "CATEGORY",
+                        payload: categoryName,
+                      });
+                      filterDispatch({
+                        type: "SET_ACTIVE_BUTTON",
+                        payload: _id,
+                      });
+                      navigate("/videos");
+                    }}
+                  >
+                    <span>{categoryName}</span>
+                  </div>
+                )
+            )}
           </div>
         </div>
-      </div>
-    </div>
+      </main>
+    </>
   );
 };
 
